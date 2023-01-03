055677abbdcfce97eee5d0a34f08d540e79cd5bd
parssak <parssak@gmail.com>
2023-01-02 23:27:37 -0500

    feat: removed router from docs header

--- src/components/docs/DocsHeader.tsx
+++ src/components/docs/DocsHeader.tsx
@@ -1,5 +1,5 @@
 import { Text } from "@parssa/universal-ui";
-import { useRouter } from "next/router";
+
 import React from "react";
 
 export const DocsHeader = ({
@@ -10,13 +10,8 @@ export const DocsHeader = ({
   title: React.ReactNode;
   description?: React.ReactNode;
 }) => {
-  const router = useRouter();
-
-  const section = router.pathname.split("/")[2];
-
   return (
     <div {...props}>
-      {/* {!(title === "Getting Started" && section === "getting-started") && <Text size='xs' className='uppercase tracking-wider text-theme-muted/80 -mb-4 relative -top-5'>{section.replace(/-/g, " ")}</Text>} */}
       <Text variant="h1" className="gradient-text leading-tight">
         {title}
       </Text>
